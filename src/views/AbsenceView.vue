55125b7311be8a3db42cca94bd93e0dd7747580b
Mohammad Aminullah <mohaminullah91@gmail.com>
2022-05-10 00:31:24 +0700

    layout absence

--- src/views/AbsenceView.vue
+++ src/views/AbsenceView.vue
@@ -30,7 +30,6 @@ async function getPeriodList() {
   try {
     const response = await axios.get("http://192.168.1.14/hris-api/api/lookupperiod");
     periodlist.value = response.data.data
-    console.log(response)
   } catch (error) {
     console.error(error)
   }
@@ -64,47 +63,68 @@ onMounted(() => {
 
 </script>
 
-
-
-
 <template>
   <Navbar />
 
-  <div class="container">
-    <h1>Absence</h1>
-    <div class="flex flex-row p-10 mx-auto">
-      <form>
-        <div class="mb-4">
-          <select v-model="selected">
-            <option disabled value="">Please select one</option>
-            <option v-for="period in periodlist" :value="period.PeriodID">
-              {{ period.Desc }}
-            </option>
-          </select>
+  <!-- Absence section start -->
+  <section id="absence" class="pb-32 pt-36 bg-slate-100">
+    <div class="container">
+      <div class="flex flex-wrap">
+        <div class="w-full px-4 mb-10 lg:w-1/2">
+          <h4 class="mb-3 text-lg font-bold uppercase text-primary">Absence Data</h4>
+          <p class="max-w-xl text-base font-medium text-secondary lg:text-lg">Silahkan pilih periode absen yang
+            diinginkan.</p>
         </div>
-        <div class="mb-4">
-          <label class="mb-1" for="period">Period</label>
-          <input placeholder="Masukkan period" id="period" type="text" name="period"
-            class="w-full py-3 mt-2 border border-gray-300 rounded-full shadow-sm px-7 focus:border-indigo-300 focus:outline-none focus:ring focus:ring-indigo-200 focus:ring-opacity-50 disabled:bg-gray-100" />
+        <div class="w-full px-4 lg:w-1/2">
+          <form>
+            <label class="mb-1" for="period">Period</label>
+            <select v-model="selected"
+              class="w-full py-3 mt-2 mb-1 border border-gray-300 rounded-full shadow-sm px-7 focus:border-indigo-300 focus:outline-none focus:ring focus:ring-indigo-200 focus:ring-opacity-50 disabled:bg-gray-100">
+              <option disabled value="">Pilih Period</option>
+              <option v-for=" period in periodlist" :value="period.PeriodID">
+                {{ period.Desc }}
+              </option>
+            </select>
+            <div class="mt-3">
+              <button @click="getData" type="button"
+                class="inline-flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white transition-colors duration-200 transform border border-transparent rounded-full bg-slate-600 hover:bg-slate-500 md:py-2 md:text-lg md:px-10 hover:shadow focus:outline-none focus:bg-slate-500">Get
+                Data</button>
+            </div>
+          </form>
         </div>
-        <div class="mb-4">
-          <label class="mb-1" for="empid">Emp ID</label>
-          <input placeholder="Masukkan Emp ID" id="empid" type="text" name="empid"
-            class="w-full py-3 mt-2 border border-gray-300 rounded-full shadow-sm px-7 focus:border-indigo-300 focus:outline-none focus:ring focus:ring-indigo-200 focus:ring-opacity-50 disabled:bg-gray-100" />
-        </div>
-        <div class="mt-6">
-          <button @click="getData" type="button"
-            class="inline-flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-full hover:bg-indigo-700 md:py-2 md:text-lg md:px-10 hover:shadow">Get
-            Data</button>
-        </div>
-      </form>
+      </div>
     </div>
-    <div v-if="dataAbsence" class="mt-4" v-for="absence in dataAbsence" :key="absence.IDNo">
-      {{ absence.Name }} - {{ absence.Day }} - {{ absence.Date }} - {{ absence.AIn }} - {{ absence.AOut }}
-      <hr>
+  </section>
+  <!-- Absence section end -->
+
+  <!-- Absence Result section start -->
+  <section id="contact" class="pb-32 pt-36 dark:bg-slate-800">
+    <div class="container">
+
+      <div class="w-full mx-auto lg:w-2/3">
+        <div class="table w-full border-collapse">
+          <div class="table-header-group border-b-2 border-gray-400 group">
+            <div class="table-row">
+              <div class="table-cell text-left">Hari</div>
+              <div class="table-cell text-left ">Tanggal</div>
+              <div class="table-cell text-left ">Absen Masuk</div>
+              <div class="table-cell text-left ">Absen Keluar</div>
+            </div>
+          </div>
+          <div class="table-row-group">
+            <div class="table-row" v-if="dataAbsence" v-for="absence in dataAbsence" :key="absence.IDNo">
+              <div class="table-cell ">{{ absence.Day }}</div>
+              <div class="table-cell ">{{ absence.Date }}</div>
+              <div class="table-cell ">{{ absence.AIn }}</div>
+              <div class="table-cell ">{{ absence.AOut }}</div>
+            </div>
+            <div v-else>
+              Data tidak ada
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div v-else>
-      Data tidak ada
-    </div>
-  </div>
+  </section>
+  <!-- Absence Result section end -->
 </template>
